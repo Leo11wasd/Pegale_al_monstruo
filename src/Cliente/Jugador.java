e379d378cc6139b2d1c1927ca9c69180ac84b611
Leopoldo Morante <98835751+Leo11wasd@users.noreply.github.com>
2025-03-16 15:14:48 -0600

    Función de la clase jugador para realizar login mediante socket tcp.

--- src/Cliente/Jugador.java
+++ src/Cliente/Jugador.java
@@ -2,8 +2,9 @@ package Cliente;
 
 import java.net.*;
 import java.io.*;
-
 import Mensajes.LoginResponse;
+import Mensajes.HitMessage;
+
 
 public class Jugador {
     Socket socket_login = null;
@@ -50,7 +51,19 @@ public class Jugador {
     }
 
 
-    public void notifica_hit() {
+    public void notifica_hit(int tiempo_logico) {
+        try {
+            socket_login = new Socket(this.valores_login.getUrl(), this.valores_login.getPuertoHit());
+            //s = new Socket("127.0.0.1", serverPort);
+            ObjectOutputStream out = new ObjectOutputStream(socket_login.getOutputStream());
+            ObjectInputStream in = new ObjectInputStream(socket_login.getInputStream());
 
+            //creo q ya no recibe nada, solamente envia
+            HitMessage mensajeHit=new HitMessage(Integer.toString(this.id_usuario),tiempo_logico);
+            out.writeObject(mensajeHit);
+        }      // UTF is a string encoding
+        catch (Exception e) {
+            System.out.println("exception:" + e.getMessage());
+        }
     }
 }
